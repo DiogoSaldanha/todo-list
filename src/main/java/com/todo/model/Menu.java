c8c3ecb4c0f79a07ef2bad0d5bdb923afe310637
DiogoSaldanha <diogossaldanha@hotmail.com>
2024-11-27 11:11:57 -0300

    Create Menu class

---
+++ src/main/java/com/todo/model/Menu.java
@@ -0,0 +1,57 @@
+package com.todo.model;
+
+import java.util.Scanner;
+
+public class Menu {
+    private Scanner scanner;
+
+    public Menu(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void showMenu() {
+        System.out.println("\nTo-Do List!");
+        System.out.println("\nMenu:\n");
+        System.out.println("1. Add a Task");
+        System.out.println("2. View Tasks");
+        System.out.println("3. Delete Tasks");
+        System.out.println("4. Mark Task as completed");
+        System.out.println("5. Exit");
+    }
+
+    public void selectChoice(int choice) {
+        switch (choice) {
+            case 1:
+                System.out.print("Enter task description: ");
+                String description = scanner.nextLine();
+                todoList.addTask(description);
+                break;
+
+            case 2:
+                todoList.viewTasks();
+                break;
+
+            case 3:
+                System.out.print("Enter task number to delete: ");
+                int deleteIndex = scanner.nextInt() - 1;
+                scanner.nextLine();
+                todoList.deleteTask(deleteIndex);
+                break;
+
+            case 4:
+                System.out.print("Enter task number to mark as completed: ");
+                int completeIndex = scanner.nextInt() - 1;
+                scanner.nextLine();
+                todoList.markAsCompleted(completeIndex);
+                break;
+
+            case 5:
+                System.out.println("Goodbye!");
+                return;
+
+            default:
+                System.out.println("Invalid choice. Try again.");
+        }
+    }
+
+}
