15beb181bbf61a93b0a18517b4ef12090adfe96a
DiogoSaldanha <diogossaldanha@hotmail.com>
2024-11-27 11:39:34 -0300

    Completing Main class

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -1,6 +1,7 @@
 package com.todo;
 
 import com.todo.model.Menu;
+import com.todo.model.TodoList;
 
 import java.util.Scanner;
 
@@ -8,13 +9,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Menu menu = new Menu(scanner);
+        TodoList todoList = new TodoList();
 
         while (true) {
             menu.showMenu();
-            int choice = scanner.nextInt();
 
+            System.out.println("\nEnter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            menu.selectChoice(choice, todoList);
             return;
         }
-
     }
 }
