82d78dc39296ba09498676ae5cf737807200e491
DiogoSaldanha <diogossaldanha@hotmail.com>
2024-11-27 11:12:27 -0300

    Create Task class

---
+++ src/main/java/com/todo/model/Task.java
@@ -0,0 +1,28 @@
+package com.todo.model;
+
+public class Task {
+    private String description;
+    private boolean completed;
+
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    @Override
+    public String toString() {
+        return (completed ? "[Completed] " : "[Pending] ") + description;
+    }
+}
